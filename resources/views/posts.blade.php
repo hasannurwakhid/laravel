9c64771b100ff8fa7ce08a0e69a77546a3983e17
hasannurwakhid <hasannurwakhid@gmail.com>
2023-05-28 17:02:50 +0700

    postingandengandatabase

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,9 +6,9 @@
             <a href="/posts/{{ $post->slug }}">
                 <h2>
                     {{ $post->title }}
-                    
                 </h2>
             </a>
+            <p>By Hasan Nur Wakhid in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <h5>
                 {{ $post->author }}
             </h5>
